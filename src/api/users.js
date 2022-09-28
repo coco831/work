f16124cc98e51472c78062166f76fd769af88392
lele <1906434287@qq.com>
2022-09-28 14:29:38 +0800

    oa项目

---
+++ src/api/users.js
@@ -0,0 +1,9 @@
+import http from "../utils/http";
+
+function login(data) {
+  return http.post('/login', data)
+}
+
+export {
+  login
+}
--- src/views/Login/Login.jsx
+++ src/views/Login/Login.jsx
@@ -1,8 +1,79 @@
 import React from 'react'
 import './Login.scss'
+import { Button, Form, Input, message } from 'antd'
+import ParticlesBg from 'particles-bg'
+import { login } from '../../api/users'
 
-export default function login() {
+export default function Login() {
+
+  const onFinish = (values) => {
+    console.log(values);
+    login(values).then((res)=>{
+      
+      message.success('登录成功');
+    }).catch((res)=>{
+      
+      message.error('登录失败');
+    })
+  }
+  const onFinishFailed = () => {}
+
   return (
-    <div>login</div>
+    <>
+      <ParticlesBg num={1} type="polygon" bg={true} color="white" />
+      <div className="Login">
+        <Form
+          name="basic"
+          labelCol={{
+            span: 8,
+          }}
+          wrapperCol={{
+            span: 16,
+          }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+        >
+          <Form.Item
+            label="邮箱"
+            name="email"
+            rules={[
+              {
+                required: true,
+                message: '请输入邮箱地址',
+              },
+              {
+                type: 'email',
+                message: '请输入正确的邮箱'
+              }
+            ]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            label="密码"
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: '密码不能为空!',
+              },
+            ]}
+          >
+            <Input.Password />
+          </Form.Item>
+          <Form.Item
+            wrapperCol={{
+              offset: 8,
+              span: 16,
+            }}
+          >
+            <Button type="primary" htmlType="submit">
+              登录
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
+    </>
   )
 }
